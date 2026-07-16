2e0d81d060456fbf08b465e40c4937b86f58b20a
Sunshine and Rainbows <himakshi.b.g.2011@gmail.com>
2026-07-16 20:30:44 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const playBtn = document.getElementById("playBtn");
+const settingsBtn = document.getElementById("settingsBtn");
+const exitBtn = document.getElementById("exitBtn");
+
+playBtn.onclick = () => {
+    alert("Level slect coming next!");
+};
+
+settingsBtn.onclick = () => {
+    alert("settings coming soon!");
+};
+
+exitBtn.onclick = () => {
+    alert("thanks for playing ghost escape");
+};
