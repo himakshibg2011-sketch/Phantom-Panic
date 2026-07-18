03fcde84e4765cdb804a0a1ff7342fb4db02fe8d
Sunshine and Rainbows <himakshi.b.g.2011@gmail.com>
2026-07-18 20:45:47 +0530

    Add new image assets for game levels and UI components
    - Added level 4, level 5, and level 5.1 images to enhance game visuals.
    - Introduced play and settings images for improved user interface.
    - Included vertical image for layout adjustments.
    - All images created using Figma for consistent design quality.

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const playBtn = document.getElementById("playBtn");
 const settingsBtn = document.getElementById("settingsBtn");
 const exitBtn = document.getElementById("exitBtn");
 
+const homeScreen = document.getElementById("homeScreen");
+const levelScreen = document.getElementById("levelScreen");
+
+const backBtn = document.getElementById("backBtn");
+
+const levelButtons = document.querySelectorAll(".levelBtn");
+
+const unlockedLevel = Number(localStorage.getItem("unlockedLevel")) || 1;
+
+updateLevels();
+
 playBtn.onclick = () => {
-    alert("Level slect coming next!");
+    homeScreen.style.display = "none";
+    levelScreen.style.display = "flex";
 };
 
 settingsBtn.onclick = () => {
@@ -13,3 +25,29 @@ settingsBtn.onclick = () => {
 exitBtn.onclick = () => {
     alert("thanks for playing ghost escape");
 };
+
+backBtn.onclick = () => {
+    levelScreen.style.display = "none";
+    homeScreen.style.display = "flex";
+};
+
+function updateLevels() {
+    levelButtons.forEach(button =>{
+        const level = Number(button.dataset.level);
+        if(level <= unlockedLevel){
+            button.classList.remove("locked");
+            button.textContent = "Level" + level;
+            button.onclick = ()=>{
+                alert("starting level" + level);
+            };
+
+        }
+        else{
+            button.classList.add("locked");
+            button.textContent = "Locked-level" + level;
+            button.onclick =()=>{
+                alert("Complete previous level first");
+            };
+        }
+    });
+}
